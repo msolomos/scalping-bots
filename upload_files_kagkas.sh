956675fba233ea6ed9912541516e45d28dd3dfe8
Gerasimos Solomos <msolomos2@gmail.com>
2025-01-02 00:46:52 +0200

    Initial Commit for Kagkas bash script to upload files automatically

---
+++ upload_files_kagkas.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Ορισμός μεταβλητών
+SFTP_USER="root"
+SFTP_HOST="192.168.1.39"
+SFTP_PORT=2244
+FILES_TO_UPLOAD=(
+    "/z/bots/scalping-bot/BITCOIN/scalper_bitcoin.py:/opt/python/scalping-bot/BITCOIN"
+    "/z/bots/scalping-bot/CARDANO/scalper_ada.py:/opt/python/scalping-bot/CARDANO"
+    "/z/bots/scalping-bot/DOGECOIN/scalper_dogecoin.py:/opt/python/scalping-bot/DOGECOIN"
+	"/z/bots/scalping-bot/ETHEREUM/scalper_ethereum.py:/opt/python/scalping-bot/ETHEREUM"
+	"/z/bots/scalping-bot/LITECOIN/scalper_litecoin.py:/opt/python/scalping-bot/LITECOIN"
+	"/z/bots/scalping-bot/POLKADOT/scalper_dot.py:/opt/python/scalping-bot/POLKADOT"
+	"/z/bots/scalping-bot/POLYGON/scalper_polygon.py:/opt/python/scalping-bot/POLYGON"
+	"/z/bots/scalping-bot/SOLANA/scalper_solana.py:/opt/python/scalping-bot/SOLANA"
+	"/z/bots/scalping-bot/STELLAR/scalper_stellar.py:/opt/python/scalping-bot/STELLAR"
+	"/z/bots/scalping-bot/XRP/scalper_xrp.py:/opt/python/scalping-bot/XRP"
+				
+    # Πρόσθεσε εδώ και τα υπόλοιπα αρχεία και διαδρομές
+)
+
+# Εκτέλεση SFTP
+sftp -P $SFTP_PORT $SFTP_USER@$SFTP_HOST <<EOF
+$(for file_mapping in "${FILES_TO_UPLOAD[@]}"; do
+    local_file=$(echo $file_mapping | cut -d':' -f1)
+    remote_path=$(echo $file_mapping | cut -d':' -f2)
+    echo "cd $remote_path"
+    echo "put $local_file"
+done)
+bye
+EOF
+
+echo "All files uploaded successfully!"
