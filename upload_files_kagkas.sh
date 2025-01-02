ec5cb5d653c3b7af2efc350cb68faa9118be06a8
Gerasimos Solomos <msolomos2@gmail.com>
2025-01-02 11:18:16 +0200

    A title and a confirmation was added to the bash script

--- upload_files_kagkas.sh
+++ upload_files_kagkas.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
+# Τίτλος
+echo "==========================="
+echo "ATTENTION: UPLOADING BOT FILES TO KAGKAS SERVER"
+echo "==========================="
+
+# Επιβεβαίωση από τον χρήστη
+read -p "Do you want to proceed with the upload? (yes/no): " user_confirmation
+
+# Έλεγχος απάντησης
+if [[ "$user_confirmation" != "yes" ]]; then
+    echo "Upload canceled by user."
+    exit 0
+fi
+
 # Ορισμός μεταβλητών
 SFTP_USER="root"
 SFTP_HOST="192.168.1.39"
 SFTP_PORT=2244
 FILES_TO_UPLOAD=(
-    "/z/bots/scalping-bot/BITCOIN/scalper_bitcoin.py:/opt/python/scalping-bot/BITCOIN"
-    "/z/bots/scalping-bot/CARDANO/scalper_ada.py:/opt/python/scalping-bot/CARDANO"
-    "/z/bots/scalping-bot/DOGECOIN/scalper_dogecoin.py:/opt/python/scalping-bot/DOGECOIN"
-	"/z/bots/scalping-bot/ETHEREUM/scalper_ethereum.py:/opt/python/scalping-bot/ETHEREUM"
-	"/z/bots/scalping-bot/LITECOIN/scalper_litecoin.py:/opt/python/scalping-bot/LITECOIN"
-	"/z/bots/scalping-bot/POLKADOT/scalper_dot.py:/opt/python/scalping-bot/POLKADOT"
-	"/z/bots/scalping-bot/POLYGON/scalper_polygon.py:/opt/python/scalping-bot/POLYGON"
+    "/z/bots/scalping-bot/AVAX/scalper_avax.py:/opt/python/scalping-bot/AVAX"
 	"/z/bots/scalping-bot/SOLANA/scalper_solana.py:/opt/python/scalping-bot/SOLANA"
+	"/z/bots/scalping-bot/LITECOIN/scalper_litecoin.py:/opt/python/scalping-bot/LITECOIN"
+	"/z/bots/scalping-bot/ETHEREUM/scalper_ethereum.py:/opt/python/scalping-bot/ETHEREUM"
+	"/z/bots/scalping-bot/BITCOIN/scalper_bitcoin.py:/opt/python/scalping-bot/BITCOIN"
+    "/z/bots/scalping-bot/XRP/scalper_xrp.py:/opt/python/scalping-bot/XRP"
+	"/z/bots/scalping-bot/CARDANO/scalper_ada.py:/opt/python/scalping-bot/CARDANO"
+    "/z/bots/scalping-bot/POLKADOT/scalper_dot.py:/opt/python/scalping-bot/POLKADOT"
+	"/z/bots/scalping-bot/DOGECOIN/scalper_dogecoin.py:/opt/python/scalping-bot/DOGECOIN"
+	"/z/bots/scalping-bot/POLYGON/scalper_polygon.py:/opt/python/scalping-bot/POLYGON"
 	"/z/bots/scalping-bot/STELLAR/scalper_stellar.py:/opt/python/scalping-bot/STELLAR"
-	"/z/bots/scalping-bot/XRP/scalper_xrp.py:/opt/python/scalping-bot/XRP"
-				
-    # Πρόσθεσε εδώ και τα υπόλοιπα αρχεία και διαδρομές
+
 )
 
 # Εκτέλεση SFTP
